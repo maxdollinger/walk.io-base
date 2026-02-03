bb0a970cefeb3ce99e7d66ab799f801ff7c703cd
Maximilian Dollinger <max@dollinger.dev>
2026-02-03 16:17:59 +0100

    add: release and versions

--- scripts/pick-latest-tag.sh
+++ scripts/pick-latest-tag.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # fetch tags without cloning full repo
-git ls-remote --tags --refs https://github.com/torvalds/linux.git |
+git ls-remote --tags --refs "$1" |
     awk '{print $2}' |
     sed 's#refs/tags/##' |
     sort -V |
